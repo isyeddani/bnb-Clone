daad7c89df221d95bc78cb10919aaefb9e4eedeb
Syed Daniyal Ahmed <syeddani23.99@gmail.com>
2023-04-03 12:02:07 +0500

    Booking Functionality Completed

--- api/index.js
+++ api/index.js
@@ -1,35 +1,36 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const User = require('./models/User'); // Creating Routes to store Users Data inside /models/Users folder
-const Place = require('./models/Place'); // Creating Routes to store Places Data inside /models/Place folder
-const cookieParser = require('cookie-parser');
-const imageDownloader = require('image-downloader');
-const multer = require('multer');
-const fs = require('fs'); //Files system library
-require('dotenv').config(); // This will load all our enviroment variables from .env
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User"); // Creating Routes to store Users Data inside /models/Users folder
+const Place = require("./models/Place"); // Creating Routes to store Places Data inside /models/Place folder
+const Booking = require("./models/Booking");
+const cookieParser = require("cookie-parser");
+const imageDownloader = require("image-downloader");
+const multer = require("multer");
+const fs = require("fs"); //Files system library
+require("dotenv").config(); // This will load all our enviroment variables from .env
 
 const app = express();
 const bcryptSalt = bcrypt.genSaltSync(10);
-tokenSecret = 'fsdafhasdjkfsdfasdkjfkla';
+tokenSecret = "fsdafhasdjkfsdfasdkjfkla";
 
 // The app.use() function adds a new middleware to the app. Essentially, whenever a request hits your backend, Express will execute the functions you passed to app.use()
 // Express.json() is a built in middleware function in Express starting from v4.16.0. It parses incoming JSON requests and puts the parsed data in req.body.
 app.use(express.json()); // Setup server to accept JSON
 app.use(cookieParser());
-app.use('/uploads', express.static(__dirname + '/uploads'));
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 
 // console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL);
 
-app.get('/test', (req, res) => {
-  res.json('test Ok');
+app.get("/test", (req, res) => {
+  res.json("test Ok");
 });
 
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   const { name, location, email, password } = req.body;
   try {
     const userDoc = await User.create({
@@ -44,7 +45,7 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const userDoc = await User.findOne({ email });
   if (userDoc) {
@@ -56,18 +57,18 @@ app.post('/login', async (req, res) => {
         {},
         (err, token) => {
           if (err) throw err;
-          res.cookie('token', token).json(userDoc);
+          res.cookie("token", token).json(userDoc);
         }
       );
     } else {
-      res.status(422).json('Pasword not Ok');
+      res.status(422).json("Pasword not Ok");
     }
   } else {
-    res.status(422).json('Not Found');
+    res.status(422).json("Not Found");
   }
 });
 
-app.get('/profile', (req, res) => {
+app.get("/profile", (req, res) => {
   const { token } = req.cookies;
   if (token) {
     jwt.verify(token, tokenSecret, {}, async (err, user) => {
@@ -80,41 +81,41 @@ app.get('/profile', (req, res) => {
   }
 });
 
-app.post('/logout', (req, res) => {
-  res.cookie('token', '').json(true);
+app.post("/logout", (req, res) => {
+  res.cookie("token", "").json(true);
 });
 
 /////////////////
 // Upload by Link
-app.post('/upload-by-link', async (req, res) => {
+app.post("/upload-by-link", async (req, res) => {
   const { Link } = req.body;
-  const newName = 'photo' + Date.now() + '.jpg';
+  const newName = "photo" + Date.now() + ".jpg";
   // console.log('Destination: ', __dirname + '/uploads/' + newName);
   await imageDownloader.image({
     url: Link,
-    dest: __dirname + '/uploads/' + newName,
+    dest: __dirname + "/uploads/" + newName,
   });
   res.json(newName);
 });
 
 ///////////////////
 // Upload by file
-const photosMiddleware = multer({ dest: 'uploads/' });
-app.post('/upload', photosMiddleware.array('photos', 100), (req, res) => {
+const photosMiddleware = multer({ dest: "uploads/" });
+app.post("/upload", photosMiddleware.array("photos", 100), (req, res) => {
   const uploadedFiles = [];
   for (let i = 0; i < req.files.length; i++) {
     const { path, originalname } = req.files[i]; // Destructure the path & originalname from the files
-    const parts = originalname.split('.');
+    const parts = originalname.split(".");
     const ext = parts[parts.length - 1]; //
-    const newPath = path + '.' + ext;
+    const newPath = path + "." + ext;
     fs.renameSync(path, newPath);
-    uploadedFiles.push(newPath.replace('uploads\\', ''));
+    uploadedFiles.push(newPath.replace("uploads\\", ""));
   }
   res.json(uploadedFiles);
 });
 
 // API for Uploading New Place Data
-app.post('/places', (req, res) => {
+app.post("/places", (req, res) => {
   const { token } = req.cookies;
   const {
     title,
@@ -152,7 +153,7 @@ app.post('/places', (req, res) => {
 });
 
 //Sending the Place Data From Database to UI , on Get'/places' Call
-app.get('/user-places', (req, res) => {
+app.get("/user-places", (req, res) => {
   const { token } = req.cookies;
   jwt.verify(token, tokenSecret, {}, async (err, user) => {
     const { id } = user;
@@ -162,14 +163,14 @@ app.get('/user-places', (req, res) => {
 
 //Sending the Place Data From Database to UI , on Get'/places/id' Call
 //This will fill the forms from the existing data on specific id
-// This will also provide Place Data to PlaceDisplay Page. 
-app.get('/places/:id', async (req, res) => {
+// This will also provide Place Data to PlaceDisplay Page.
+app.get("/places/:id", async (req, res) => {
   const { id } = req.params;
   res.json(await Place.findById(id));
 });
 
 //Allow to Update the Place Data of id
-app.put('/places', async (req, res) => {
+app.put("/places", async (req, res) => {
   const { token } = req.cookies;
   const {
     id,
@@ -201,13 +202,33 @@ app.put('/places', async (req, res) => {
         price,
       });
       placeDoc.save();
-      res.json('OK');
+      res.json("OK");
     }
   });
 });
 
-app.get('/places', async (req, res) => {
+app.get("/places", async (req, res) => {
   res.json(await Place.find());
+});
+
+app.post("/bookings", (req, res) => {
+  const { place, checkIn, checkOut, numberOfGuest, name, mobile, price } =
+    req.body;
+  Booking.create({
+    place: place,
+    checkIn,
+    checkOut,
+    numberOfGuest,
+    name,
+    mobile,
+    price,
+  })
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      throw err;
+    });
 });
 
 app.listen(4000);
--- client/src/BookingCard.jsx
+++ client/src/BookingCard.jsx
@@ -1,29 +1,99 @@
-export default function BookingCard({place}){
-    return (
-        <div className="bg-white shadow p-4 rounded-2xl h-fit">
-          <h2 className="text-xl text-center mb-1">
-            Price: ${place.price}/per night{' '}
-          </h2>
-          <div className="border rounded-2xl mt-4">
-            <div className="flex">
-              <div className="px-3 py-4">
-                <label>Check in: </label>
-                <input type="date"></input>
-              </div>
-              <div className="px-3 py-4 border-l">
-                <label>Check out: </label>
-                <input type="date"></input>
-              </div>
-            </div>
-            <div className="flex">
-              <div className="px-3 py-4 border-t">
-                <label>Number of guests:</label>
-                <input type="number" value={1}></input>
-              </div>
-            </div>
-          </div>
+import { useState } from "react";
+import { compareAsc, differenceInCalendarDays, format } from "date-fns";
+import axios from "axios";
+import { Navigate } from "react-router-dom";
 
-          <button className="primary mt-4">Book this place</button>
+export default function BookingCard({ place }) {
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numberOfGuest, setNumberOfGuest] = useState(1);
+  const [name, setName] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [redirect,setRedirect] = useState("");
+  let numberOfNights = 0;
+  // Calculate the Number of Nights between two dates.
+  if (checkIn && checkOut) {
+    numberOfNights = differenceInCalendarDays(
+      new Date(checkOut),
+      new Date(checkIn)
+    );
+  }
+
+  async function bookPlace() {
+    const response = await axios.post("/bookings", {
+      place: place._id,
+      checkIn,
+      checkOut,
+      numberOfGuest,
+      name,
+      mobile,
+      price: numberOfNights * place.price,
+    });
+    const bookingid = response.data._id
+    setRedirect(`/account/bookings/${bookingid}`);
+  }
+
+  if(redirect){
+    return <Navigate to={redirect} />
+  }
+
+  return (
+    <div className="bg-white shadow p-4 rounded-2xl h-fit">
+      <h2 className="text-xl text-center mb-1">
+        Price: ${place.price}/per night{" "}
+      </h2>
+      <div className="border rounded-2xl mt-4">
+        <div className="flex">
+          <div className="px-3 py-4">
+            <label>Check in: </label>
+            <input
+              type="date"
+              value={checkIn}
+              onChange={(ev) => setCheckIn(ev.target.value)}
+            ></input>
+          </div>
+          <div className="px-3 py-4 border-l">
+            <label>Check out: </label>
+            <input
+              type="date"
+              value={checkOut}
+              onChange={(ev) => setCheckOut(ev.target.value)}
+            ></input>
+          </div>
         </div>
-    )
+        <div className="px-3 py-4 border-t">
+          <label>Number of guests:</label>
+          <input
+            type="number"
+            value={numberOfGuest}
+            onChange={(ev) => setNumberOfGuest(ev.target.value)}
+          ></input>
+        </div>
+        {numberOfNights > 0 && (
+          <div className="px-3 py-4 border-t">
+            <label>Your full name</label>
+            <input
+              type="text"
+              placeholder="Daniyal Ahmed"
+              value={name}
+              onChange={(ev) => setName(ev.target.value)}
+            ></input>
+            <label>Your phone number</label>
+            <input
+              type="tel"
+              placeholder=""
+              value={mobile}
+              onChange={(ev) => setMobile(ev.target.value)}
+            ></input>
+          </div>
+        )}
+      </div>
+      <button onClick={bookPlace} className="primary mt-4">
+        Book this place
+        {numberOfNights > 0 && (
+          <span> for :${numberOfNights * place.price}</span>
+        )}
+      </button>
+    </div>
+  );
 }
